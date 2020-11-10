a507f21b6188f91ee07e92b1dfc94ecb800aa1ce
Uliana <ulidmitrieva97@gmail.com>
2020-11-10 23:57:58 +0100

    add ErrorBound

--- src/components/people-page/people-page.js
+++ src/components/people-page/people-page.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 
 import SwapiService from "../../services/swapi-service";
-import { ErrorIndicator } from "../error-indicator";
+import ErrorBounary from "../error-boundary";
 import ItemList from "../item-list";
 import PersonDetails from "../person-details";
 import { Row } from "../row";
@@ -13,38 +13,32 @@ export default class PeoplePage extends Component {
 
   state = {
     selectedItem: 5,
-    error: false,
   };
-
-  //Basic ErrorBounary in lifecykle. Ловит ошибки РЕНДЕРИНГА (state, render comp.did.mount ...) от всех компонентов снизу! Благодаря ему остальная часть приложения, не задетая ошибкой все еще сможет работать!
-  //Принимает 2 аргумента error and info. -> debugger -> console -> sources
-  componentDidCatch() {
-    debugger;
-    this.setState({ error: true });
-  }
 
   onItemSelected = (id) => {
     this.setState({ selectedItem: id });
   };
 
   render() {
-    if (this.state.error) {
-      return <ErrorIndicator />;
-    }
-
     const itemList = (
-      <ItemList
-        onItemSelected={this.onItemSelected}
-        //Мы делаем этот компонент универсальным для людей, кораблей и планет благодаря тому, что мы вынесли отдельно вызов к серверу, который в последствии позвратит промис. Теперь мы можем контролировать извне то, какие данные будет получать и отображать компонент изменяя getAll...
-        //Здесь важно не потерять значение this у метода getAllPeople -> использовать ф стрелку!
-        getData={this.swapiService.getAllPeople}
-        //мы принимаем каждый объект массива и отображаем то, что нам нужно
-      >
-        {(i) => `${i.name} (${i.gender})`}
-      </ItemList>
+      <ErrorBounary>
+        <ItemList
+          onItemSelected={this.onItemSelected}
+          //Мы делаем этот компонент универсальным для людей, кораблей и планет благодаря тому, что мы вынесли отдельно вызов к серверу, который в последствии позвратит промис. Теперь мы можем контролировать извне то, какие данные будет получать и отображать компонент изменяя getAll...
+          //Здесь важно не потерять значение this у метода getAllPeople -> использовать ф стрелку!
+          getData={this.swapiService.getAllPeople}
+          //мы принимаем каждый объект массива и отображаем то, что нам нужно
+        >
+          {(i) => `${i.name} (${i.gender})`}
+        </ItemList>
+      </ErrorBounary>
     );
 
-    const personDetails = <PersonDetails itemId={this.state.selectedItem} />;
+    const personDetails = (
+      <ErrorBounary>
+        <PersonDetails itemId={this.state.selectedItem} />
+      </ErrorBounary>
+    );
 
     return (
       <>
